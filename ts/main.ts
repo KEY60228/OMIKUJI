efa6295b91646ff00dc69771613e527b24e92dc1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-05-26 22:44:53 +0900

    finished (using array)

--- ts/main.ts
+++ ts/main.ts
@@ -4,27 +4,8 @@
   const btn: HTMLElement = document.getElementById('js-btn');
 
   btn.addEventListener('click', () => {
-    const n: number = Math.floor(Math.random() * 5);
-    
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '吉';
-        break;
-      case 3:
-        btn.textContent = '小吉';
-        break;
-      case 4:
-        btn.textContent = '末吉';
-        break;
-      case 5:
-        btn.textContent = '凶';
-        break;
-    }
+    const results: string[] = ['大吉', '中吉', '吉', '小吉', '末吉', '凶']
+    const n: number = Math.floor(Math.random() * results.length);
+    btn.textContent = results[n];
   });
 }
